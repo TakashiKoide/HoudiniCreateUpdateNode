da5960b294805d1659db6d96fd3d301846089584
TakashiKoide <50489494+TakashiKoide@users.noreply.github.com>
2019-12-08 22:19:43 +0900

    MultiParmに対応
    node_type.parmTemplates()ではMultiParmを取得出来てなかったので、再帰処理で取得するように変更。

--- python2.7libs/create_update_node/get_node_data.py
+++ python2.7libs/create_update_node/get_node_data.py
@@ -2,6 +2,15 @@
 import hou
 import os
 
+def get_all_parm_templates(all_parms, node_type):
+    parms = node_type.parmTemplates()
+    for parm in parms:
+        if parm.type() == hou.parmTemplateType.Folder:
+            get_all_parm_templates(all_parms, parm)
+        elif parm.type() != hou.parmTemplateType.FolderSet:
+            all_parms.append(parm)
+    return all_parms
+
 def main():
     node_data = {}
     categories = hou.nodeTypeCategories()
@@ -13,8 +22,8 @@ def main():
             node_info = {}
             node_info['node_name'] = node_name
             node_info['node_label'] = node_type.description()
-            parms = node_type.parmTemplates()
-            node_info['parms'] = [parm.name() for parm in parms]
+            all_parms = get_all_parm_templates([], node_type)
+            node_info['parms'] = [parm.name() for parm in all_parms]
             category_data.append(node_info)
 
         node_data[category_name] = category_data
